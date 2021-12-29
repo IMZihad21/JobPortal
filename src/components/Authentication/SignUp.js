9ad8009809c9652e414fb449b06c58f6957c8316
ZèD <imzihad@gmail.com>
2021-12-29 12:49:12 +0600

    Auth: Basic structure of signin and signup components
    Signed-off-by: ZèD <imzihad@gmail.com>

---
+++ src/components/Authentication/SignUp.js
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function SignUp() {
+    const [ gender, setGender ] = React.useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="full-name"
+                                name="fullName"
+                                required
+                                fullWidth
+                                id="fullName"
+                                label="Full Name"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="phoneNumber"
+                                label="Phone Number"
+                                name="phoneNumber"
+                                type="tel"
+                                autoComplete="phone-number"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="birthDate"
+                                label="Date of Birth"
+                                type="date"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <FormControl required fullWidth>
+                                <InputLabel id="demo-simple-select-required-label">Gender</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-required-label"
+                                    id="demo-simple-select-required"
+                                    value={gender}
+                                    label="Gender *"
+                                    onChange={(e) => setGender(e.target.value)}
+                                >
+                                    <MenuItem value="male">Male</MenuItem>
+                                    <MenuItem value="female">Female</MenuItem>
+                                    <MenuItem value="others">Others</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="confirmPassword"
+                                id="confirmPassword"
+                                autoComplete="confirm-password"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign Up
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
+}
--- src/components/Authentication/index.js
+++ src/components/Authentication/index.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
-const Authentication = () => {
+export default function Authentication() {
+
     return (
-        <div>
-            auth
-        </div>
-    )
+        <Grid container component="main" sx={{ height: '600px' }}>
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <SignUp />
+            </Grid>
+            <Grid item xs={false} sm={4} md={7}>
+                <Box sx={{ textAlign: 'left', mt: "100px", mx: "200px" }}>
+                    <Typography variant="h4" color="primary.main">
+                        Tech
+                    </Typography>
+                    <Typography variant="h2" color="secondary.main" sx={{ fontWeight: "500" }}>
+                        Foring
+                    </Typography>
+                    <Typography variant="subtitle2" color="primary.main">
+                        Shaping tomorrow's Cyber Security
+                    </Typography>
+                </Box>
+                <Box sx={{ textAlign: "center", mt: "50px", mx: "50px" }}>
+                    <Typography variant="h4" color="primary.main">
+                        Welcome to TechForing
+                    </Typography>
+                    <Typography variant="h6" color="secondary.main">
+                        Notice:
+                    </Typography>
+                    <Typography variant="h6" color="primary.main">
+                        An Applicant can only register once.
+                    </Typography>
+                    <Typography variant="subtitle1" color="primary.main">
+                        Registered users, please login with your credentials by entering your email and password.
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
+    );
 }
-
-export default Authentication
