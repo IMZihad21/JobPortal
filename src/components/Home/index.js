50c507d78f6eaec80e815913394ec414666b9a6e
ZèD <imzihad@gmail.com>
2021-12-30 13:47:41 +0600

    Home: Added simple home component
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,29 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 export default function Home(props) {
     return (
         <Container>
-            <Typography align="center" variant="h4" component="h1" gutterBottom>
-                Welcome to the React Material-UI
-            </Typography>
+            <Box sx={{ textAlign: 'left', mt: "100px", display: "flex", justifyContent: "center" }}>
+                <Box>
+                    <Typography variant="h4" color="primary.main">
+                        Tech
+                    </Typography>
+                    <Typography variant="h2" color="secondary.main" sx={{ fontWeight: "500" }}>
+                        Foring
+                    </Typography>
+                    <Typography variant="subtitle2" color="primary.main">
+                        Shaping tomorrow's Cyber Security
+                    </Typography>
+                </Box>
+            </Box>
+            <Box sx={{ textAlign: "center", mt: "50px", mx: "50px" }}>
+                <Typography variant="h4" color="primary.main">
+                    Welcome to TechForing
+                </Typography>
+            </Box>
         </Container>
     );
 }
